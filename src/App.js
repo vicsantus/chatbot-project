487880e24fc0804a71ed378719652a0699da21f4
victrosantos <victor.santos.fk@hotmail.com>
2023-07-17 12:32:42 -0300

    feat: Adicionado comentarios explicativos a App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ import Chat from './pages/Chat';
 
 export const About = () => <h1>Você está na página Sobre</h1>;
 export const NoMatch = () => <h1>Página não encontrada</h1>;
+// Componente funcional que renderiza a página de "Página não encontrada"
 
 export default function App() {
   return (
     <div>
+      {/* Navegação por links usando o componente "Link" do react-router-dom */}
       <Link to="/">Início</Link>
       <br />
       <Link to="/about">Sobre</Link>
+
+      {/* Switch para renderizar apenas a primeira Route que corresponda ao caminho */}
       <Switch>
+        {/* Rota exata para a página principal, renderiza o componente Chat */}
         <Route exact path="/" component={Chat} />
+
+        {/* Rota para a página "Sobre", renderiza o componente funcional "About" */}
         <Route path="/about" component={About} />
+
+        {/* Rota padrão para quando nenhuma outra rota corresponder, renderiza o componente funcional "NoMatch" */}
         <Route component={NoMatch} />
       </Switch>
     </div>
