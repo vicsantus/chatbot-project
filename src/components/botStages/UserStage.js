75441a587e5465983c35dd9d58bc76f65d3d858d
victrosantos <victor.santos.fk@hotmail.com>
2023-07-17 12:02:36 -0300

    feat: Adicionado comentarios explicativos a UserStage.js

--- src/components/botStages/UserStage.js
+++ src/components/botStages/UserStage.js
@@ -9,10 +9,14 @@ function UserStage({MakeCSV}) {
   const {conversationStage, setConversationStage} = useContext(ChatContext);
   const {setCheckUser} = useContext(ChatContext);
 
-
+  // Utilização do hook useEffect para realizar ações quando o estado de textData muda
   useEffect(() => {
+    // A função dentro do useEffect será executada toda vez que o estado de textData mudar
     if (conversationStage === 'userStage' && textData[textData.length - 1].who === 'user') {
+      // Verificação se a etapa da conversa é 'userStage' e se a última mensagem é do usuário
+
       if (!['hello', 'good', 'i want', 'goodbye'].includes(atualUser.toLowerCase())) {
+        // Se a entrada do usuário não for 'hello', 'good', 'i want', ou 'goodbye'
         setCheckUser({
           user: atualUser.toLowerCase()
         });
@@ -26,12 +30,14 @@ function UserStage({MakeCSV}) {
             msg: 'Ok! Now please enter your password!'
           }
         ]);
-        setConversationStage('checkStage');
+        setConversationStage('checkStage'); // Define a próxima etapa da conversa como 'checkStage'
 
       } else if (['hello', 'good', 'i want'].includes(atualUser.toLowerCase())) {
-        setConversationStage('start');
+        // Se a entrada do usuário for 'hello', 'good', ou 'i want'
+        setConversationStage('start'); // Define a próxima etapa da conversa como 'start'
 
       } else if ('goodbye' === atualUser.toLowerCase()) {
+        // Se a entrada do usuário for 'goodbye'
         setTextData([ 
           ...textData,
           {
@@ -41,11 +47,12 @@ function UserStage({MakeCSV}) {
             msg: 'Goodbye! To the next!'
           }
         ]);
-        MakeCSV(textData);
-        setConversationId(conversationId + 1);
-        setConversationStage('start');
+        MakeCSV(textData); // Chama a função MakeCSV passando o array textData como argumento
+        setConversationId(conversationId + 1); // Incrementa o conversationId e atualiza o estado
+        setConversationStage('start'); // Define a próxima etapa da conversa como 'start' novamente
 
       } else if (atualUser) {
+        // Se a entrada do usuário não corresponder a nenhum caso acima
         setTextData([ 
           ...textData,
           {
