b7ef6eb3b791f89e383ddd19eff4f05b5d46fab5
victrosantos <victor.santos.fk@hotmail.com>
2023-07-18 15:20:45 -0300

    feat: Add comentarios a Header e About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,26 +1,32 @@
+// Importa as bibliotecas necessárias
+import '@testing-library/jest-dom/extend-expect';
 import { screen, waitFor } from '@testing-library/react';
 import React from 'react';
-// import { act } from 'react-dom/test-utils';
-import '@testing-library/jest-dom/extend-expect';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 import ChatProvider from '../context/ChatProvider';
 import { renderWithRouter } from './helpers/renderWith';
 
+// Descreve o grupo de testes do componente About
 describe('Testa o componente About', () => {
+  // Teste 1: Verifica se a rota About existe corretamente
   test('1-Testa se a rota About existe corretamente', async () => {
+    // Renderiza o componente App dentro do ChatProvider e utiliza a função renderWithRouter para testar as rotas
     const { history } = renderWithRouter(
       <ChatProvider>
         <App />
       </ChatProvider>,
     );
 
+    // Utiliza a função act para realizar uma ação assíncrona de navegação para a rota '/about'
     act(() => {
       history.push('/about');
     });
 
+    // Obtém o elemento do texto 'About Page' na tela
     const inputMsg = screen.getByText(/About Page/i);
 
+    // Aguarda até que o elemento 'About Page' seja exibido na tela
     await waitFor(() => {
       expect(inputMsg).toBeInTheDocument();
     });
--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,3 +1,4 @@
+// Importa as bibliotecas necessárias
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
@@ -6,19 +7,29 @@ import App from '../App';
 import ChatProvider from '../context/ChatProvider';
 import { renderWithRouter } from './helpers/renderWith';
 
+// Descreve o grupo de testes do componente Header
 describe('Testa o componente Header', () => {
+  // Teste 1: Testa o Header Geral
   test('1-Testa o Header Geral', async () => {
+    // Renderiza o componente App dentro do ChatProvider e utiliza a função renderWithRouter para testar as rotas
     const { history } = renderWithRouter(
       <ChatProvider>
         <App />
       </ChatProvider>,
     );
+
+    // Utiliza a função act para realizar uma ação assíncrona de navegação para a rota '/about'
     act(() => {
       history.push('/about');
     });
 
+    // Obtém o botão de texto 'HOME' na tela
     const btnHome = screen.getByText(/HOME/i);
+
+    // Simula um clique no botão 'HOME'
     userEvent.click(btnHome);
+
+    // Aguarda até que o elemento do input de mensagem seja exibido na tela
     await waitFor(() => {
       const inputMsg = screen.getByRole('textbox');
       expect(inputMsg).toBeInTheDocument();
