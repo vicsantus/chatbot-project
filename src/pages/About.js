ad70515af29ef97099e06f41cf7d6738d618b299
victrosantos <victor.santos.fk@hotmail.com>
2023-07-18 15:29:47 -0300

    fix: Corrigido about h1 no lugar errado

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,13 +4,15 @@ const About = () => {
   // Componente funcional "About" representa a página "About" do chatbot de empréstimos
 
   return (
-    <div style={{color: '#fff'}} className='about'>
-      <h1>About Page</h1>
-      <p>This chatbot was created by Victor Santos for a Lexart Labs.</p>
-      <p>The chatbot is designed to handle loan requests and provide information about loan conditions.</p>
-      <p>It interacts with users and guides them through the loan application process.</p>
-      <p>Feel free to explore and interact with the chatbot to experience its functionalities!</p>
-    </div>
+    <>
+      <h1 style={{color: '#fff'}}>About Page</h1>
+      <div style={{color: '#fff'}} className='about'>
+        <p>This chatbot was created by Victor Santos for a Lexart Labs.</p>
+        <p>The chatbot is designed to handle loan requests and provide information about loan conditions.</p>
+        <p>It interacts with users and guides them through the loan application process.</p>
+        <p>Feel free to explore and interact with the chatbot to experience its functionalities!</p>
+      </div>
+    </>
   );
 };
 
